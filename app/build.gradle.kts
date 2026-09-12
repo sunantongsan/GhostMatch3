3be748d3848eef23d9a4bcbf8b7aa7ad74157b8c
sunantongsan <104156645+sunantongsan@users.noreply.github.com>
2026-09-12 09:33:51 +0700

    Prepare illustrated artwork test APK v0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.sunantongsan.ghostmatch3"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
     }
 
     buildTypes {
