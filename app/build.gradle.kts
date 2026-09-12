262583ea6cdc1e1ee24c044668999001223470b6
sunantongsan <104156645+sunantongsan@users.noreply.github.com>
2026-09-12 10:10:39 +0700

    Publish richer Ghost Match 3 gameplay test v0.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.sunantongsan.ghostmatch3"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.5.0"
+        versionCode = 6
+        versionName = "0.6.0"
     }
 
     buildTypes {
