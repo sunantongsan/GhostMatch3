d2a2488725a9320078445855245198ca1380cea6
sunantongsan <104156645+sunantongsan@users.noreply.github.com>
2026-09-13 12:57:10 +0700

    Release v0.7.0 magical power-up build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.sunantongsan.ghostmatch3"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.6.0"
+        versionCode = 7
+        versionName = "0.7.0"
     }
 
     buildTypes {
