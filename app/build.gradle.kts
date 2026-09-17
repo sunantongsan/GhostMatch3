3da78db96b51863ab761c9836d587c1144e32a40
sunantongsan <104156645+sunantongsan@users.noreply.github.com>
2026-09-17 16:26:24 +0700

    Prepare v0.16.0 onboarding and icon test APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.sunantongsan.ghostmatch3"
         minSdk = 24
         targetSdk = 35
-        versionCode = 15
-        versionName = "0.15.0"
+        versionCode = 16
+        versionName = "0.16.0"
     }
 
     buildTypes {
