8a31aee2e9b21e8bafdd886fd1596e56a4578599
sunantongsan <104156645+sunantongsan@users.noreply.github.com>
2026-09-17 16:00:34 +0700

    Prepare v0.15.0 progressive levels test APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.sunantongsan.ghostmatch3"
         minSdk = 24
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.14.0"
+        versionCode = 15
+        versionName = "0.15.0"
     }
 
     buildTypes {
