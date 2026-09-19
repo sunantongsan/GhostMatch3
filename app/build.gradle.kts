2a2b9bd3afc03d974d0ecac3315175090248047b
sunantongsan <104156645+sunantongsan@users.noreply.github.com>
2026-09-19 19:24:50 +0700

    Bump Android version to 0.26.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.sunantongsan.ghostmatch3"
         minSdk = 24
         targetSdk = 35
-        versionCode = 25
-        versionName = "0.25.0"
+        versionCode = 26
+        versionName = "0.26.0"
     }
 
     buildTypes {
